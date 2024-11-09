db21661de7ad10ea0c494f81133b2593b8981bcb
Nicolas Granatelli Sanches <nicolassanches-ieg@instituto.ad>
2024-11-08 22:01:58 -0300

    mobile otimizado

--- app/src/main/java/com/example/monetizemais/TelaWebView.java
+++ app/src/main/java/com/example/monetizemais/TelaWebView.java
@@ -1,21 +1,13 @@
 package com.example.monetizemais;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.KeyEvent;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class TelaWebView extends AppCompatActivity {
     private WebView webView;
-    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +15,14 @@ public class TelaWebView extends AppCompatActivity {
         setContentView(R.layout.activity_tela_web_view);
 
         webView = findViewById(R.id.webview);
-
-        handler = new Handler();
-
-        handler.postDelayed(checkInternetRunnable, 10000);
-
         loadWebView();
-
-        webView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadWebView();
-            }
-        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        loadWebView();
+    private void loadWebView() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("https://front-end-wfl2.onrender.com/");
     }
 
     @Override
@@ -53,32 +33,4 @@ public class TelaWebView extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
-
-    private boolean verificaRede() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager != null) {
-            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-        }
-        return false;
-    }
-
-    // Runnable para verificar a conectividade à internet periodicamente
-    private Runnable checkInternetRunnable = new Runnable() {
-        @Override
-        public void run() {
-            loadWebView();
-            // Agendar a próxima verificação após 10 segundos
-            handler.postDelayed(this, 10000);
-        }
-    };
-
-    private void loadWebView() {
-        if (verificaRede()) {
-                WebSettings webSettings = webView.getSettings();
-                webSettings.setJavaScriptEnabled(true);
-                webSettings.setDomStorageEnabled(true);
-                webView.loadUrl("https://front-end-wfl2.onrender.com/");
-            }
-        }
-    }
+}
